0cdc002b0448c2acced2be1eef0fde1469adb9e0
Manuel Duarte <carlosd99m@gmail.com>
2021-12-18 01:00:36 -0600

    project init

---
+++ Compatibility/Xamarin.Forms/Renderers/Renderers.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using XamarinApp = Renderers.App;
+
+namespace Renderers.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new XamarinApp());
+        }
+    }
+}
---
+++ Handlers/.NET MAUI/CustomHandlers/CustomHandlers/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using CustomHandlers.Handlers;
+
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace CustomHandlers
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+           
+            builder    
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
+#if __ANDROID__
+                    handlers.AddHandler(typeof(CustomEntry), typeof(CustomEntryHandler));
+#endif
+                });
+            
+            return builder.Build();
+        }
+    }
+}
